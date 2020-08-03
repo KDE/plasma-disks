6700327471efbf861bc9677a7858fcba9a7a1631
Harald Sitter <sitter@kde.org>
2020-08-03 16:28:39 +0200

    make translation domain more generic
    in theory there may well be other systems than smart in the future

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,2 +1,5 @@
-#! /bin/sh
-$XGETTEXT *.cpp -o $podir/kded_smart.pot
+#!/bin/sh
+# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-FileCopyrightText: 2020 Harald Sitter <sitter@kde.org>
+
+$XGETTEXT *.cpp -o $podir/plasma_disks.pot
