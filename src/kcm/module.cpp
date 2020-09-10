a0516b9d7cd01e0af60655c11305c1f8265efa64
Harald Sitter <sitter@kde.org>
2020-09-10 19:35:42 +0200

    add missing licensing tags to trivial files
    we've discussed this at akademy and clarified the policy to always use
    cc0 for trivial files. apply that to desktop-y files and test fixtures
    
    logo.png is actually from breeze-icons, so tag that properly as lgpl

--- src/kcm/module.cpp
+++ src/kcm/module.cpp
@@ -10,6 +10,37 @@
 #include "servicerunner.h"
 #include "version.h"
 
+//--------------------------------------------------
+#include <QObject>
+#include <QDBusConnection>
+#include <QDBusMessage>
+class StatusBlob : public QObject
+{
+    Q_OBJECT
+public:
+    using QObject::QObject;
+
+public Q_SLOTS:
+    bool smbd()
+    {
+        auto msg = QDBusMessage::createMethodCall(QStringLiteral("org.freedesktop.systemd1"),
+                                                  QStringLiteral("/org/freedesktop/systemd1/unit/smbd_2eservice"),
+                                                  QStringLiteral("org.freedesktop.DBus.Properties.Get"),
+                                                  QStringLiteral("org.freedesktop.systemd1.Unit"));
+        msg << QStringLiteral("ActiveState");
+        auto reply = QDBusConnection::systemBus().call(msg);
+        if (reply.type() == QDBusMessage::ErrorMessage) {
+            return false;
+        }
+        qDebug() << reply.arguments();
+        // if we have a service returned then it must have found it
+        return !reply.arguments().isEmpty();
+    }
+
+    }
+};
+//--------------------------------------------------
+
 Module::Module(QObject *parent, const QVariantList &args)
     : KQuickAddons::ConfigModule(parent, args)
 {
@@ -43,3 +74,5 @@ void Module::save()
 void Module::defaults()
 {
 }
+
+#include "module.moc"
