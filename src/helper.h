d245a094a09ee5fc2c255b46bd50d25d780327f6
Laurent Montel <montel@kde.org>
2023-06-04 16:18:35 +0200

    We depend against kf6 5.250

--- src/helper.h
+++ src/helper.h
@@ -3,14 +3,9 @@
 
 #ifndef HELPER_H
 #define HELPER_H
-#include <QObject>
-#include <kauth_version.h>
-#if KAUTH_VERSION >= QT_VERSION_CHECK(5, 92, 0)
 #include <KAuth/ActionReply>
 #include <KAuth/HelperSupport>
-#else
-#include <KAuth>
-#endif
+#include <QObject>
 
 using namespace KAuth;
 
--- src/smartctl.cpp
+++ src/smartctl.cpp
@@ -3,18 +3,13 @@
 
 #include "smartctl.h"
 
-#include <kauth_version.h>
-#if KAUTH_VERSION >= QT_VERSION_CHECK(5, 92, 0)
 #include <KAuth/Action>
 #include <KAuth/ExecuteJob>
 #include <KAuth/HelperSupport>
-#else
-#include <KAuthAction>
-#include <KAuthExecuteJob>
-#endif
 #include <KLocalizedString>
 #include <QDebug>
 #include <QFileInfo>
+#include <kauth_version.h>
 
 #include "kded_debug.h"
 #include "smartfailure.h"
