c5e22caaaa34c3243cd4ebf23f2b655a53757fb4
Ivan Čukić <ivan.cukic@kde.org>
2026-06-06 08:46:49 +0200

    Add missing qqml.h include
    qmlRegisterType and related functions live in qqml.h

--- src/kcm/module.cpp
+++ src/kcm/module.cpp
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 // SPDX-FileCopyrightText: 2020 Harald Sitter <sitter@kde.org>
 
+#include <qqml.h>
+
 #include <KQuickConfigModule>
 
 #include "devicemodel.h"
